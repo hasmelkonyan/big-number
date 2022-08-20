7043fb5314bc8315d1547472933d5231bcf4bbc1
Hasmik Melkonyan <82414047+hasmelkonyan@users.noreply.github.com>
2022-08-20 22:56:00 +0400

    Update big_number.py

--- big_number.py
+++ big_number.py
@@ -47,3 +47,36 @@ def big_number_plus(str1, str2):
 
 print(big_number_plus("1234560", "66694300"))
 print(1234560 + 66694300)
+
+
+def big_number_minus(str1, str2):
+    res = []
+    lst1, lst2 = str_to_lst(str1), str_to_lst(str2)
+    if len(lst1) > len(lst2):
+        for i in range(len(lst1) - len(lst2)):
+            lst2.insert(0, "000")
+    elif len(lst2) > len(lst1):
+        for i in range(len(lst2) - len(lst1)):
+            lst1.insert(0, "000")
+    is_one = 0
+    for i in range(len(lst1) - 1, -1, -1):
+        lst1[i] = str(int(lst1[i]) - is_one)
+        if int(lst1[i]) < int(lst2[i]):
+            lst1[i] = str(int(lst1[i]) + 1000)
+            num = int(lst1[i]) - int(lst2[i])
+            res.insert(0, str(num))
+            is_one = 1
+        else:
+            num = int(lst1[i]) - int(lst2[i])
+            res.insert(0, str(num))
+            is_one = 0
+    for i in range(len(res)):
+        if len(res[i]) == 2:
+            res[i] = "0" + res[i]
+        elif len(res[i]) == 1:
+            res[i] = "00" + res[i]
+    return res
+
+
+print(2598599 - 2589)
+print(big_number_minus("2598599", "2589"))
